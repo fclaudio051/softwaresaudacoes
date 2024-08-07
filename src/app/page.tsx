79f595de51247faef3728226a3dd0b5c681b0151
CLAUDIO FAUSTINO PEREIRA <71899539+fclaudio051@users.noreply.github.com>
2024-08-07 09:47:44 -0300

    redução de código

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,15 @@ const Page = () => {
   }).format();
 
 const hour = new Date().getHours();
-let greeting = '';
-
-if(hour >= 0 && hour < 12) {
-  greeting = "Bom dia ☀️";
-} else if (hour >= 12 && hour < 18) {
-  greeting = "Boa tarde 🌅";
-} else if (hour >= 18 && hour <= 23) {
-  greeting = 'Boa noite 🌃';
-}
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
       <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl font-bold">{greeting}</div>
+      <div className="text-5xl font-bold">
+      {hour >= 0 && hour <12 && "Bom dia☕"}
+      {hour >= 12 && hour <18 && "Boa tarde🌅"}
+      {hour >= 18 && hour <=23 && "Boa noite🌕"}
+      </div>
     </div>
   );
 }
